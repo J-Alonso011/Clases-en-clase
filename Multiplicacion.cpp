2ee1ab4e04308c3ad2d851ea41a68caef6146752
Your NamePedroBamaca <pbamacac@miumg.edu.gt>
2025-03-16 12:23:00 -0600

    Multiplicacion

---
+++ Multiplicacion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Multiplicacion{
+	private :
+		string cui,nombres,apellidos;
+		
+		void crar();
+		void leer();
+		void actualizar();
+		void borrar();
+};
